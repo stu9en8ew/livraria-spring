6b3abeb255e02bfd4c8a2e121db7f7ce1fd963bd
Marcio <mar.info.rj@gmail.com>
2017-07-13 08:09:31 -0300

    Estudos sobre Jsf e Spring

---
+++ src/main/java/br/com/caelum/livraria/modelo/Usuario.java
@@ -0,0 +1,47 @@
+package br.com.caelum.livraria.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="UsuarioJ")
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String email;
+	private String senha;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
